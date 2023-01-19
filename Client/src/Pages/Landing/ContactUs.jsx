930a944d60044bb87ffe4348b0a0cc57bb4e6dae
Rachel N. Biesiedzinski <85037264+beeinkks@users.noreply.github.com>
2023-01-19 11:38:50 -0500

    contact us page layout done

--- Client/src/Pages/Landing/ContactUs.jsx
+++ Client/src/Pages/Landing/ContactUs.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+
 import NavBar from "./../../Components/LandingNav/LandingNav"
+
+import Logo from "./../../Components/Images/face.png";
 
 
 export const ContactUs = (props) => {
@@ -10,8 +13,19 @@ export const ContactUs = (props) => {
             <NavBar />
             
             <div className="contact">
-                <h1>Contact Now!</h1>
+                <h1>Contact Us</h1>
+                <p> Email: BearMaxTeam@BearMax.com</p>
+                <p> Call: 1-800-BEAR-MAX</p>
+            </div>
 
+            <h1>Meet The Team</h1>
+            <div className="meatTeam">
+                <img src={Logo} className="smLandLogo" alt="Logo: bear max face" />
+                <img src={Logo} className="smLandLogo" alt="Logo: bear max face" />
+                <img src={Logo} className="smLandLogo" alt="Logo: bear max face" />
+                <img src={Logo} className="smLandLogo" alt="Logo: bear max face" />
+                <img src={Logo} className="smLandLogo" alt="Logo: bear max face" />
+                <img src={Logo} className="smLandLogo" alt="Logo: bear max face" />
             </div>
         </div>
     )
