3c156bdf3f76695af43351432e386159155ed09f
Rachel N. Biesiedzinski <85037264+beeinkks@users.noreply.github.com>
2023-02-13 18:51:30 -0500

    fixed registers function

--- Client/src/Components/Register.js
+++ Client/src/Components/Register.js
@@ -1,38 +1,69 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import {buildPath } from "./BuildPath";
+
 import Logo from "./Images/face.png";
 
 function Register ()
 {
     const navigate = useNavigate();
 
-    const [firstName, setFName] = useState('');
-    const [lastName, setLName] = useState('');
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    var firstName;
+    var lastName;
+    var email;
+    var pass;
+    var reType;
 
-    async function registerUser (event)
-    {
+    let bp = require("./BuildPath.js");
+
+    const registerUser = async (event) => {
         event.preventDefault();
 
-        const response = await fetch('http://localhost:1337/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({
-                firstName,
-                lastName,
-                email,
-                pass
-            })
-        });
+        var obj = {
+            FirstName: firstName.value,
+            LastName: lastName.value,
+            Email: email.value,
+            Password: pass.value
+        };
 
-        const data = await response.json();
+        var js = JSON.stringify(obj);
 
-        console.log(data);
-    }
+        if (
+            firstName.value === "" ||
+            lastName.value  === "" ||
+            email.value     === "" ||
+            pass.value      === "" 
+        ) {
+            //setMessage("Please fill in all feilds.");
+            return; 
+        } else if (pass.value.length < 8) {
+            //setMessage ( "Please make sure the pasword is at least 8 characters long.");
+            return;
+        } else if (reType.value !== pass.value) {
+            //setMessage ("Passwords don't match.");
+            return ; 
+        } else {
+            try {
+                const response = await fetch(buildPath("api/register"), {
+                    method: "POST",
+                    body: js,
+                    header: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                    },
+                });
+
+                var res = JSON.parse(await response.text());
+
+                //setMessage(" ");
+                window.location.href="/login";
+            } catch (e) {
+                alert(e.toString());
+                return ;
+            }
+        }
+    };
 
     return (
         <div className="Register">
@@ -41,15 +72,17 @@ function Register ()
 
             <form onSubmit={registerUser}>
                 
-                <input type="text" className="regText" id="regName" placeholder="First Name" value={firstName} onChange={(e) => setFName(e.target.value)}/> <br />
+                <input type="text" className="regText" placeholder="First Name"  ref={(c) => (firstName = c)}/> <br />
                 
-                <input type="text" placeholder="Last Name" className="regText" value={lastName} onChange={(e) => setLName(e.target.value)}  /> <br />
+                <input type="text" placeholder="Last Name" className="regText" ref={(c) => (lastName = c)}  /> <br />
 
-                <input value={email} className="regText" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="email@bearmax.com" /> <br />
+                <input value={email} className="regText" ref={(c) => (email = c)} type="email" placeholder="email@bearmax.com" /> <br />
 
-                <input  value={pass} className="regText" onChange={(e) => setPass(e.target.value)}type="password" placeholder="Password" /> <br />
+                <input  value={pass} className="regText" ref={(c) => (pass = c)} type="password" placeholder="Password" /> <br />
+
+                <input  value={pass} className="regText" ref={(c) => (reType = c)} type="password" placeholder="Retype Password" /> <br />
                 
-                <input className="regButton" type="submit" value="Register" /> <br />
+                <input className="regButton" type="submit" value="Register" onClick={registerUser}/> <br />
             </form>
             <a className="regToLog" onClick={() => navigate("/login")}><u>Already have an account? Login!</u></a> 
         </div>
