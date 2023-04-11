9ef14df86c41d82399fe6b928183fb8cca692ac3
Rachel N. Biesiedzinski <85037264+beeinkks@users.noreply.github.com>
2023-04-11 17:42:29 -0400

    Layout Complete for How to Use

--- Client/src/Pages/Landing/HowToUse.jsx
+++ Client/src/Pages/Landing/HowToUse.jsx
@@ -4,6 +4,11 @@ import NavBar from "./../../Components/LandingNav/LandingNav"
 import Logo from "./../../Components/Images/face.png";
 import Bear from "./../../Components/Images/full.png";
 
+import Aone from "./../../Components/Images/catAOne.png";
+import Atwo from "./../../Components/Images/catATwo.png";
+import Bone from "./../../Components/Images/catBOne.png";
+import Btwo from "./../../Components/Images/catBTwo.png";
+
 export const HowToUse = (props) => {
 
     return (
@@ -13,8 +18,8 @@ export const HowToUse = (props) => {
             
 
            <div className="howToUse">
-            <img src={Bear} className="useBear" alt="Design of bearmax, brown simple bear" />
-                <div className="textUse">
+                <img src={Bear} className="useBear" alt="Design of bearmax, brown simple bear" />
+                <div className="textUse1">
                     <h1 className="useHead">How to Use BearMAX</h1>
                     <p>Currently, Bearmax is capable of two modes, important to social-emotional learning facilitation: </p>
                         <ul>
@@ -24,7 +29,7 @@ export const HowToUse = (props) => {
 
                     <h3>For the "Mirror Image" Game</h3>
                         <p>"Mirror Image" helps your child to recognize and mimic emotions portrayed by the robot.</p>
-                        <ul>
+                        <ol>
                             <li>To start the game, open the Bearmax app and log in</li>
                             <li>After logging in, click "(I forgot button names)" to start the "Mirror Image" Game.</li>
                             <li>When "Mirror Image" begins, Bearmax will portray an emotion. This emotion will be either
@@ -37,32 +42,43 @@ export const HowToUse = (props) => {
                                 button "(I forgot button names)" is pressed for the game to stop.</li>
                             <li>Check your results here, on the website, when completed! These results will tell you how many 
                                 emotions your child "mirrored" correctly.</li>
-                        </ul>
+                        </ol>
+                </div>
 
-                    <h3>For Stress Detection Mode: </h3>
-                        <p>Stress detection is a built-in, passive feature that can be used alongside Bearmax when having access 
-                            to both the wearable device and website. This mode is currently recommended for sessions with a 
-                            clinician.</p>
-                        <ul>
-                            <li>To begin stress detection mode, have your child wear the device on the wrist of their non-dominant 
-                                hand. </li>
-                            <li>Connect the electrodes from the device to the following locations on the child's hand (based on 
-                                their comfort). You can either copy Image Category A or Image Category B for electrode placement. 
-                                Image A: Electrodes placed at the palm of the hand Image B: Electrodes placed at the index and 
-                                middle fingers </li>
-                            <li>Turn on the wearable device. </li>
-                            <li>Log in to your account on the website. </li>
-                            <li>On the website, click "(I forgot button names)" on the Dashboard (main page after logging in) 
-                                to connect the wearable device to the platform. (6) If successful, data will begin to populate 
-                                a real-time graph for review on the Dashboard. If unsuccessful, tips will be provided on how 
-                                to re-connect the device.</li>
-                            <li>Click "(I forgot button names)" to stop data collection from the Wearable Device and turn it off.</li>
-                            <li>Save the data to your computer by clicking "Save.”</li>
-                        </ul>
+                <div className="container">
+                    <div className="catElement">
+                        <h6>Category A:</h6> 
+                            <img src={Aone} className="catOne" alt="..." />
+                            <img src={Atwo} className="catOne" alt="..." /> 
+                        <h6>Category B:</h6>
+                            <img src={Bone} className="catTwo" alt="..." />
+                            <img src={Btwo} className="catTwo" alt="..." />
+                    </div>
+
+                    <div className="textUse2">
+                        <h3>For Stress Detection Mode: </h3>
+                            <p>Stress detection is a built-in, passive feature that can be used alongside Bearmax when having access 
+                                to both the wearable device and website. This mode is currently recommended for sessions with a 
+                                clinician.</p>
+                            <ol>
+                                <li>To begin stress detection mode, have your child wear the device on the wrist of their non-dominant 
+                                    hand. </li>
+                                <li>Connect the electrodes from the device to the following locations on the child's hand (based on 
+                                    their comfort). You can either copy Image Category A or Image Category B for electrode placement. 
+                                    Image A: Electrodes placed at the palm of the hand Image B: Electrodes placed at the index and 
+                                    middle fingers </li>
+                                <li>Turn on the wearable device. </li>
+                                <li>Log in to your account on the website. </li>
+                                <li>On the website, click "(I forgot button names)" on the Dashboard (main page after logging in) 
+                                    to connect the wearable device to the platform. (6) If successful, data will begin to populate 
+                                    a real-time graph for review on the Dashboard. If unsuccessful, tips will be provided on how 
+                                    to re-connect the device.</li>
+                                <li>Click "(I forgot button names)" to stop data collection from the Wearable Device and turn it off.</li>
+                                <li>Save the data to your computer by clicking "Save.”</li>
+                            </ol>
+                    </div>
                 </div>
            </div>
-            
-            
         </div>
     )
 }
