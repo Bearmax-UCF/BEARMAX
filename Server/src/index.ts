5393319fb5a1cc107ff89368d3808b8dee991eec
ElijahMSmith <mcditto99@gmail.com>
2023-04-12 16:27:26 -0400

    Routing to build folder

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -12,23 +12,43 @@ const io = new Server(server);
 import setupAuthSvc from "./services/auth";
 import registerHandlers from "./eventHandlers";
 import routes from "./routes";
+import path from "path";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Setup MongoDB Connection
-const dbClient = mongoose.connect(constants.mongo_uri).then((m) => {
-	console.log("CONNECTED TO MONGODB");
-	return m.connection.getClient();
-});
+// // Setup MongoDB Connection
+// const dbClient = mongoose.connect(constants.mongo_uri).then((m) => {
+// 	console.log("CONNECTED TO MONGODB");
+// 	return m.connection.getClient();
+// });
 
-// Setup Auth (Passport & Sessions)
-// @ts-ignore
-setupAuthSvc(app, dbClient);
+// // Setup Auth (Passport & Sessions)
+// // @ts-ignore
+// setupAuthSvc(app, dbClient);
 
 // Setup Routes
 app.use("/", routes);
+
+// Serve the frontend app
+if (constants.isProduction) {
+	app.use(
+		"/static",
+		express.static(path.join(__dirname, "../../Client/build/static/"))
+	);
+	app.get("*", function (_, res) {
+		res.sendFile("index.html", {
+			root: path.join(__dirname, "../../Client/build/"),
+		});
+	});
+} else {
+	app.use(
+		"*",
+		createProxyMiddleware({ target: "http://localhost:3000", ws: true })
+	);
+}
 
 // Setup socketio
 registerHandlers(io);
--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -1,27 +1,9 @@
 import { Router } from "express";
-import path from "path";
 import apiRoutes from "./api";
 const router = Router();
-import {createProxyMiddleware} from "http-proxy-middleware";
-import constants from "../utils/constants";
 
 router.use("/api", apiRoutes);
 // fallback 404
 router.use("/api", (_, res) => res.status(404));
-
-// Serve the frontend app
-if (constants.isProduction) {
-  router.get("/", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "..", "..", "Client", "build", "index.html"))
-  });
-  router.get("*", (req, res) => {
-    const filePath = req.path.split("/")
-    res.sendFile(path.resolve(__dirname, "..", "..", "..", "Client", "build", ...filePath))
-  });
-} else {
-  router.use("*",
-    createProxyMiddleware({ target: "http://localhost:3000", ws: true })
-  );
-}
 
 export default router;
