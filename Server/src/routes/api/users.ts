fb436b06b9bf230b900f957cdf05d1929afd2605
ElijahMSmith <mcditto99@gmail.com>
2023-04-20 18:03:12 -0400

    /me status

--- Server/src/routes/api/users.ts
+++ Server/src/routes/api/users.ts
@@ -4,20 +4,20 @@ import User from "../../models/User";
 const router = Router();
 
 router.get("/me", requireJwtAuth, (req, res) => {
-  const me = req.user!;
-  res.json({
-    me
-  });
+	const me = req.user!;
+	res.status(200).json({
+		me,
+	});
 });
 
 router.delete("/:id", requireJwtAuth, async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndRemove(req.params.id);
+	try {
+		const user = await User.findByIdAndRemove(req.params.id);
 
-    res.status(200).json({ user });
-  } catch (err) {
-    next(err);
-  }
+		res.status(200).json({ user });
+	} catch (err) {
+		next(err);
+	}
 });
 
 export const basePath = "/users";
